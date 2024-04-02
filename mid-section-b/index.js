e4d8c3595d17c0fb3716aea13998e6a8a7dd53eb
= <parkash.official1308@gmail.com>
2024-04-02 14:55:52 +0500

    css and html

---
+++ mid-section-b/index.js
@@ -0,0 +1,11 @@
+const img = document.getElementsByClassName("container");
+var index = 0;
+
+function getNextImage() {
+    if ()
+        document.getElementById("currentImg").src = img[index].src;
+}
+
+function getPrevImage() {
+    document.getElementById("currentImg").src = img[0].src;
+}
